b82871810ea44151f9259cf88b9779b205e94193
Vidlyra <funhigh924@gmail.com>
2026-07-24 07:23:02 +0530

    Create anime-data.js

---
+++ Vidlyra-watch/js/anime-data.js
@@ -0,0 +1,69 @@
+const animeLibrary = [
+
+{
+    id:"unexpected",
+
+    title:"Unexpected",
+
+    genre:"Action • Sci-Fi",
+
+    year:"2026",
+
+    rating:"9.8",
+
+    status:"Coming Soon",
+
+    banner:"assets/images/Unexpected.png",
+
+    poster:"assets/images/anime poster.png",
+
+    description:"A mysterious event changes the destiny of an entire world.",
+
+    episodes:12
+},
+
+{
+    id:"hollow-frequency",
+
+    title:"Hollow Frequency",
+
+    genre:"Sci-Fi",
+
+    year:"2026",
+
+    rating:"9.6",
+
+    status:"Coming Soon",
+
+    banner:"assets/images/hollow frequency code zero.png",
+
+    poster:"assets/images/featured banner.png",
+
+    description:"The signal has returned. Humanity must answer.",
+
+    episodes:10
+},
+
+{
+    id:"crimson-velocity",
+
+    title:"Crimson Velocity",
+
+    genre:"Action",
+
+    year:"2026",
+
+    rating:"9.5",
+
+    status:"Coming Soon",
+
+    banner:"assets/images/Crimson velocity.png",
+
+    poster:"assets/images/thumbnail.png",
+
+    description:"Speed alone cannot save the future.",
+
+    episodes:8
+}
+
+];
